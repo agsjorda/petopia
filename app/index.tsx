12f26148954b4d6ad4ed8e26e3b689806246912f
agsjorda <agsjorda@gmail.com>
2024-08-09 02:45:03 +0800

    Initial Project setup with native wind

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex items-center justify-center h-full"
+    >
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
