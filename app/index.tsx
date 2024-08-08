4dd091f16cbe92d1852b922dcf9b854334ca1f14
agsjorda <agsjorda@gmail.com>
2024-08-09 02:59:09 +0800

    added outfit font

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,11 @@ export default function Index() {
     <View
       className="flex items-center justify-center h-full"
     >
-      <Text>Hello World!</Text>
+      <Text style={{
+        fontFamily: "outfit",
+        fontSize: 24,
+        color: "black",
+      }}>Hello World!</Text>
     </View>
   );
 }
